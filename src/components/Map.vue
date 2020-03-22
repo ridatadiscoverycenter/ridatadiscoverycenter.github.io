6157f33004a68d527922d9f982fff30dea95310e
fernandogelin <fernando@fernandogelin.com>
2020-03-22 09:27:06 -0400

    feat: 🎸 add buoy markers to map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,18 +1,33 @@
 <template>
-  <MglMap
-    :accessToken="accessToken"
-    :mapStyle.sync="mapStyle"
-    :center.sync="mapCenter"
-    :zoom="9"
-  >
-  </MglMap>
+  <div>
+    <MglMap
+      :accessToken="accessToken"
+      :mapStyle.sync="mapStyle"
+      :center.sync="mapCenter"
+      :zoom="9"
+      :minZoom="9"
+    >
+      <div v-for="(point, index) in resources.buoy.data" v-bind:key="index">
+        <MglMarker :coordinates="[point.longitude, point.latitude]">
+          <BuoyIcon slot="marker" class="icon" />
+        </MglMarker>
+      </div>
+    </MglMap>
+  </div>
 </template>
 
 <script>
+import BuoyIcon from '@/assets/illustrations/buoy-marker.svg'
+import { mapState } from 'vuex'
 import Mapbox from 'mapbox-gl'
-import { MglMap } from 'vue-mapbox'
+import { MglMap, MglMarker } from 'vue-mapbox'
 
 export default {
+  components: {
+    MglMap,
+    MglMarker,
+    BuoyIcon
+  },
   created() {
     // We need to set mapbox-gl library here in order to use it in template
     this.mapbox = Mapbox
@@ -25,9 +40,6 @@ export default {
       onMediaChange(e)
     })
   },
-  components: {
-    MglMap
-  },
   data() {
     return {
       mapCenter: [-71.374022, 41.577553],
@@ -36,6 +48,7 @@ export default {
     }
   },
   computed: {
+    ...mapState(['resources']),
     mapStyle: function() {
       return this.darkScheme
         ? process.env.VUE_APP_MAP_STYLE_DARK
