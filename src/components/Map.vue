d5f5620cc7e41a1e772f7b06332adcff09ea52c2
fernandogelin <fernando@fernandogelin.com>
2020-03-22 12:09:48 -0400

    fix: 🐛 fix media query match

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -12,43 +12,39 @@
         <MglMarker :coordinates="[point.longitude, point.latitude]">
           <font-awesome-icon slot="marker" icon="circle" :color="markerColor" />
           <MglPopup>
-            <VCard>
+            <div class="">
               <div class="">
-                <div class="">
-                  <div class="media">
-                    <BuoyIcon class="buoy-icon" />
-                    <div class="media-content">
-                      <p class="title is-4">{{ point.name }}</p>
-                      <p class="subtitle">
-                        <span class="buoy-labels"
-                          >label: {{ point.label }}</span
-                        >
-                        <span class="buoy-labels"
-                          >BART: {{ point.BART_label }}</span
-                        >
-                      </p>
-                    </div>
-                  </div>
-
-                  <div class="content buoy-info">
-                    <p class="buoy-descriptor">
-                      <strong>type: </strong>{{ point.station_type }}
+                <div class="media">
+                  <BuoyIcon class="buoy-icon" />
+                  <div class="media-content">
+                    <p class="title is-4">{{ point.name }}</p>
+                    <p class="subtitle">
+                      <span class="buoy-labels">label: {{ point.label }}</span>
+                      <span class="buoy-labels"
+                        >BART: {{ point.BART_label }}</span
+                      >
                     </p>
-                    <p class="buoy-descriptor">
-                      <strong>owner: </strong>{{ point.owner }}
-                    </p>
-                    <p class="buoy-descriptor">
-                      <strong>operator: </strong>{{ point.operator }}
-                    </p>
-                    <p class="buoy-descriptor">
-                      <strong>season: </strong>{{ point.season }}
-                    </p>
-                    <br />
-                    <time>{{ point.dates_stabilisehd }}</time>
                   </div>
                 </div>
+
+                <div class="content buoy-info">
+                  <p class="buoy-descriptor">
+                    <strong>type: </strong>{{ point.station_type }}
+                  </p>
+                  <p class="buoy-descriptor">
+                    <strong>owner: </strong>{{ point.owner }}
+                  </p>
+                  <p class="buoy-descriptor">
+                    <strong>operator: </strong>{{ point.operator }}
+                  </p>
+                  <p class="buoy-descriptor">
+                    <strong>season: </strong>{{ point.season }}
+                  </p>
+                  <br />
+                  <time>{{ point.dates_stabilisehd }}</time>
+                </div>
               </div>
-            </VCard>
+            </div>
           </MglPopup>
         </MglMarker>
       </div>
@@ -84,16 +80,14 @@ export default {
     return {
       mapCenter: [-71.374022, 41.577553],
       accessToken: process.env.VUE_APP_MAPBOX_ACCESS_TOKEN, // access token. Needed if you using Mapbox maps
-      darkScheme: window.matchMedia('(prefers-color-scheme: dark)')
-        ? true
-        : false,
-      markerColor: window.matchMedia('(prefers-color-scheme: dark)')
-        ? '#FEBE80'
-        : '#025E73'
+      darkScheme: window.matchMedia('(prefers-color-scheme: dark)').matches
     }
   },
   computed: {
     ...mapState(['resources']),
+    markerColor: function() {
+      return this.darkScheme ? '#FEBE80' : '#025E73'
+    },
     mapStyle: function() {
       return this.darkScheme
         ? process.env.VUE_APP_MAP_STYLE_DARK
