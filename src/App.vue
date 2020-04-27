8bbb556a7f9ac84089c31abaf607e072909b6ed7
fernandogelin <fernando@fernandogelin.com>
2020-04-27 14:02:47 -0400

    style: 💄 style changes

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,26 @@
     <main id="app" class="application-grid">
       <BaseNavbar class="application-grid-header" />
       <section>
-        <router-view />
+        <transition name="slide-fade" mode="out-in">
+          <router-view />
+        </transition>
       </section>
     </main>
   </div>
 </template>
 
 <script></script>
+<style lang="sass">
+.slide-fade-enter
+  transform: translateX(10px)
+  opacity: 0
+
+.slide-fade-enter-active,
+.slide-fade-leave-active
+  transition: all 0.2s ease
+
+
+.slide-fade-leave-to
+  transform: translateX(-10px)
+  opacity: 0
+</style>
