0a1e56230f824df30b24db7c808077f188f0d877
Timothy Divoll <timothy.divoll@gmail.com>
2025-08-28 16:54:50 -0400

    updates to use latest vue2 with node22

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,14 @@ module.exports = {
     '@vue/prettier',
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
   },
   overrides: [
     {
--- src/main.js
+++ src/main.js
@@ -15,14 +15,14 @@ import camelCase from 'lodash/camelCase'
 const requireComponent = require.context(
   './components',
   false,
-  /Base[A-Z]\w+\.(vue|js)$/
+  /Base[A-Z]\w+\.(vue|js)$/,
 )
 
 requireComponent.keys().forEach((fileName) => {
   const componentConfig = requireComponent(fileName)
 
   const componentName = upperFirst(
-    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1')),
   )
 
   Vue.component(componentName, componentConfig.default || componentConfig)
